a260a4ae0aa07d60443956c532209e14e5973e12
Kelley A Park <42754532+kpark526@users.noreply.github.com>
2022-05-12 14:08:08 -0400

    Added different game modes

---
+++ solitaire_menu.py
@@ -0,0 +1,47 @@
+import pygame
+import time
+from card import Card
+from pile import Pile
+import random
+import os
+
+if __name__ == "__main__":
+   pygame.init()
+   
+   # display of 1400 by 800 pixels
+   scene = pygame.display.set_mode((1120,800))
+   pygame.display.set_caption("Solitaire Menu")
+   
+   color = (255,255,255)
+   boxColor = (255,234,236)
+   boxColor2 = (243,154,157)
+   width = scene.get_width()
+   height = scene.get_height()
+   fonty = pygame.font.SysFont('Arial',35)
+   welcomeFont = pygame.font.SysFont('Arial',50)
+   easyText = fonty.render('Freestyle Solitaire',True,(0,0,0))
+   hardText = fonty.render('"Classic" Solitaire',True,color)
+   welcomeText = welcomeFont.render('Pick a Mode',True,color)
+   
+   running = True
+   while running:
+      for event in pygame.event.get():
+         if event.type == pygame.QUIT:
+            running = False
+         elif event.type == pygame.MOUSEBUTTONDOWN:
+            if width/2 < mouse[0] < width/2+325 and height/2 <= mouse[1] < height/2+40:
+               import solitaire_easy
+               running = False
+               pygame.quit()
+               sys.exit()
+            elif width/2 - 400 < mouse[0] < width/2+325-400and height/2 <= mouse[1] < height/2+40:
+               running = False
+      
+      scene.fill([63,108,81])
+      mouse = pygame.mouse.get_pos()
+      pygame.draw.rect(scene,boxColor,[width/2,height/2,325,40])
+      scene.blit(easyText, (width/2+50,height/2))
+      pygame.draw.rect(scene,boxColor2,[width/2-400,height/2,325,40])
+      scene.blit(hardText,(width/2+50-400,height/2))
+      scene.blit(welcomeText,(width/2+50-200,height/2-200))
+      pygame.display.update()
